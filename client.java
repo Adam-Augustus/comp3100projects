e962fac456e2410f1e317d03100c3b1eb45d07b5
Daniel Johnston <daniel.johnston@students.mq.edu.au>
2023-05-27 13:45:55 +1000

    best fit

--- client.java
+++ client.java
@@ -1,4 +1,4 @@
-import java.net.*;  
+import java.net.*;
 import java.io.*; 
 
 public class client{
@@ -19,9 +19,9 @@ public class client{
         try {
             //open connection
             s = new Socket(a, p);
-            System.out.println("socket opened!");
-            System.out.println("Local IP: " + s.getLocalAddress() + " Local port:" + s.getLocalPort());
-            System.out.println("Remote IP: " + s.getInetAddress() + " Remote port:" + s.getPort());
+            //System.out.println("socket opened!");
+            //System.out.println("Local IP: " + s.getLocalAddress() + " Local port:" + s.getLocalPort());
+            //System.out.println("Remote IP: " + s.getInetAddress() + " Remote port:" + s.getPort());
 
             din = new BufferedReader(new InputStreamReader(s.getInputStream()));
             dout = new DataOutputStream(s.getOutputStream());
@@ -36,7 +36,8 @@ public class client{
 
     public void run() throws IOException{
     String str;             //String to hold job requests when buffer is overwritten (such as when executing GETS)
-    System.out.println("Handshake success?: " + handshake());
+    //System.out.println("Handshake success?:)) " + handshake());
+    handshake();
 
     dataSend("REDY");
 
@@ -44,29 +45,28 @@ public class client{
 
     //getServers();
 
-    int serverToUse = 0;
     while (!str.equals("NONE")) {
         String[] split = str.split(" ");
         if (split[0].equals("OK")) {
             dataSend("REDY");
         }
+        //Read job data into variables
         if (split[0].equals("JOBN")) {
-            //TODO get job requirements to find suitable server
-            //create function to find best server for job
-            System.out.println("Rec Job " + str);
-            int subTime = Integer.parseInt(split[1]);
-            int jobID = Integer.parseInt(split[2]);
-            int estRuntime = Integer.parseInt(split[3]);
+            //System.out.println("Rec Job " + str);
+            //int subTime = Integer.parseInt(split[1]);
+            //int jobID = Integer.parseInt(split[2]);
+            //int estRuntime = Integer.parseInt(split[3]);
             int reqCores = Integer.parseInt(split[4]);
             int reqMem = Integer.parseInt(split[5]);
             int reqDisk = Integer.parseInt(split[6]);
 
             getCapServer(reqCores, reqMem, reqDisk); //fill currServer array with servers capable of given job
-            String a = findSmallestServerForJob(estRuntime, reqCores, reqMem, reqDisk); //find name of smallest server capable of executing job
-            int b = compServers(a); //find server with lowest waiting jobs
+            int thisServer = findBestServerForJob(reqCores, reqMem, reqDisk);
+            String serverToUse = currServers[thisServer].serverType;
+            int serverNoToUse = currServers[thisServer].serverID;
 
-            dataSend("SCHD " + split[2] + " " + a + " " + b);
-            System.out.println("Scheduled job " + jobID + " to " + a + " " + b);
+            dataSend("SCHD " + split[2] + " " + serverToUse + " " + serverNoToUse);
+            //System.out.println("Scheduled job " + jobID + " to " + serverToUse + " " + serverNoToUse);
         }
         if (split[0].equals("JCPL")) {
             dataSend("REDY");
@@ -151,28 +151,33 @@ public class client{
         dataSend("OK");
     }
 
-    String findSmallestServerForJob(int run, int core, int mem, int disk) {
-        //find smallest server that can run job
-        String x = currServers[0].serverType;
-        for (int i = 0; i < noOfCurrServers-1; i++) {
-            if (currServers[i+1].cores < currServers[i].cores) x = currServers[i+1].serverType;
-        }
-        return x;
-    }
+    int findBestServerForJob(int reqCores, int reqMem, int reqDisk) {
+        //modified (improved) version of a Best Fit algorithm. Not currently using the global Queue.
+        //get cap server with lowest 'working score' ("req / avail") (aka fitness score)
+        //assign job to that server
+        //--
+        //score needs to be averaged, i.e. a large server running at 50% has to have a lower score than a tiny server running 60%
+        //needs to return an Int that represents an index to chosen server in currServers.
+        //--
+        //cores is cores avaliable to use not avaliable overall
+        int bestScore = 99999999; //start best score functionally infinite
+        int serverToUse = 0;
+        for (int i = 0; i < noOfCurrServers; i++) {
+            //create a fitness score for each server based on amount of jobs
+            int check = 0; //initialize check as 0
+            check = check + currServers[i].rJobs + 10000; //valuate having running jobs as super unfavourable, all jobs same weight
+            check = check + currServers[i].wJobs; //valuate waiting jobs regularly
 
-    String findBestServerForJob() {
-        //compare cores mem and disk
-        return null;
-    }
+            //normalize score based on avaliable cores in server (server with more cores can have more jobs in queue)
 
-    int compServers(String a) {
-        int x = currServers[0].serverID;
-        for (int i = 0; i < noOfCurrServers-1; i++) {
-            if (currServers[i+1].serverType.equals(a)) {
-                if (currServers[i+1].wJobs < currServers[i].wJobs) x = currServers[i+1].serverID;
+
+            //System.out.println("Checking server " + currServers[i].serverType + " " + currServers[i].serverID + " with score " + Check);
+            if (check < bestScore) {
+                bestScore = check;
+                serverToUse = i;
             }
         }
-        return x;
+        return serverToUse;
     }
 
     //write s to given dout, and print to console (removed print for submission)
